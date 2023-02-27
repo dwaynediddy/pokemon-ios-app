fa83e42b59d16c42e22df6bb5e28c4092bd87c89
dwaynediddy <burrows.dwayne@icloud.com>
2023-02-27 10:16:06 +0100

    Initial Commit

---
+++ pokemon-ios-app/pokemon_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_ios_appApp.swift
+//  pokemon-ios-app
+//
+//  Created by Dwayne on 27/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
